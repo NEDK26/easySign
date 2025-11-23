ba5e149474f89dfc95e31f06b4527326f72708a3
minda <1049696396@qq.com>
2025-11-23 15:00:38 +0800

    commit：new project 开发 starter

---
+++ ip-counter-booter-starter/src/main/java/com/mila/ipcounterbooterstarter/IpCounterBooterStarterApplication.java
@@ -0,0 +1,13 @@
+package com.mila.ipcounterbooterstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpCounterBooterStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpCounterBooterStarterApplication.class, args);
+	}
+
+}
---
+++ ip-counter-booter-starter/src/test/java/com/mila/ipcounterbooterstarter/IpCounterBooterStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mila.ipcounterbooterstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpCounterBooterStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
